e01d5cd96bf2f0cbc8c04008ad6258c09ea9cf29
cleo <cleo24620@gmail.com>
2024-04-09 21:09:57 +0800

    get the fgl_split_ids which satisfies the cond_x, cond_y and cond_bx. It can use for spliting.

--- funcs.py
+++ funcs.py
@@ -6,6 +6,7 @@
 from pathlib import Path
 import cdflib
 import pandas as pd
+import numpy as np
 
 # get my current path
 dir_p = Path('.')
@@ -15,10 +16,12 @@ cdf_fs = [f for f in dir_p.glob('*.cdf')]
 for f in cdf_fs:
     print(f)
 
+
 def pre_cdf(f):
     """
     print the necessary information
-    :return cdf_file
+    :return cdf_file:
+    :return data: remove empty data
     """
     cdf_file = cdflib.CDF(f)
     variables = cdf_file.cdf_info().zVariables
@@ -46,4 +49,41 @@ def formatted_time(timestamp):
     # 这里 "%Y-%m-%d %H:%M:%S" 是一个常见的格式，表示年-月-日 时:分:秒
     # 你可以根据需要调整这个格式
     formatted_time = dt_object.strftime("%Y-%m-%d %H:%M:%S")
-    print(formatted_time)
+    return formatted_time
+
+
+def get_con_subs(time_series, diff=60):
+    # 计算相邻时间点之间的差异（单位为天）
+    diffs = np.diff(time_series)
+
+    # 识别连续性断点（这里假设连续的定义是每天一个数据点，断点大于1天）
+    breaks = np.where(diffs > diff)[0] + 1
+
+    # 分割时间序列
+    continuous_subsequences = np.split(time_series, breaks)
+
+    return continuous_subsequences
+
+
+def split_array(arr, chunk_size=20):
+    """将列表分割成指定大小的多个子列表"""
+    # 计算应保留的元素总数（剔除最后不满20个元素的部分）
+    num_elements_to_keep = (arr.size // chunk_size) * chunk_size
+
+    # 截取数组以保留完整的子数组部分
+    trimmed_arr = arr[:num_elements_to_keep]
+
+    # 分割数组为每20个元素一个子数组
+    subarrays = np.split(trimmed_arr, num_elements_to_keep // 20)
+
+    return subarrays
+
+
+def check_same_sign(arr):
+    # 检查是否全为正数
+    all_positive = np.all(arr > 0)
+    # 检查是否全为负数
+    all_negative = np.all(arr < 0)
+
+    # 如果全为正或全为负，则不含异号元素
+    return all_positive or all_negative
